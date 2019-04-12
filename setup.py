367af5fffbc3f955b812b0dc1f1bd078b5944b69
Tommy Mckinnon <admin@hyperevo.com>
2019-04-12 14:28:22 -0700

    fixed compatability with new web3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import (
 
 setup(
     name='helios_web3',
-    version='5.0.3',
+    version='5.0.4',
     description="""Helios version of Web3.py""",
     long_description_markdown_filename='README.rst',
     author='Tommy Mckinnon',
