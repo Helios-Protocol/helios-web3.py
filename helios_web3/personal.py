e148c99a8ed399d7c1ece4821ec53a29d306984d
Tommy Mckinnon <admin@hyperevo.com>
2019-08-01 10:38:39 -0700

    Added personal sendTransactions and receiveTransactions. Version bump

--- helios_web3/personal.py
+++ helios_web3/personal.py
@@ -1,8 +1,19 @@
 from web3.geth import GethPersonal
-
+from web3.method import (
+    Method,
+    default_root_munger,
+)
 
 class Personal(GethPersonal):
     '''
-    Class to allow for overriding functions if nessisary
+    Class to allow for overriding functions if necessary
     '''
-    pass
+    sendTransactions = Method(
+        "personal_sendTransactions",
+        mungers=[default_root_munger],
+    )
+
+    receiveTransactions = Method(
+        "personal_receiveTransactions",
+        mungers=[default_root_munger],
+    )
